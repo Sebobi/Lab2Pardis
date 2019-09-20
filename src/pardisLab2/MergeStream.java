e833fbcb03d98569dba02770aab4116adcccc1db
miguel <miguelmu@kth.se>
2019-09-20 12:19:05 +0200

    remove middle

--- src/pardisLab2/MergeStream.java
+++ src/pardisLab2/MergeStream.java
@@ -51,10 +51,8 @@ public class MergeStream {
     for (int currSize = 1; currSize < 2 * n; currSize *= 2) {
       int[][] grouped = new int[(int)Math.ceil(n / (2.0 * currSize))][2 * currSize];
       for (int i = 0, left = 0; left < n; i++, left += 2 * currSize) {
-        int middle = Math.min(left + currSize - 1, n - 1);
         int right = Math.min(left + (2 * currSize) - 1, n - 1);
 
-        // grouped[i] = Arrays.copyOfRange(array, left, right + 1);
         System.arraycopy(array, left, grouped[i], 0, right - left + 1);
 
         // Special case for last array
@@ -64,10 +62,10 @@ public class MergeStream {
           }
         }
       }
-      array = Arrays.copyOfRange(Arrays.asList(grouped).parallelStream().flatMapToInt(group -> {
+      array = Arrays.asList(grouped).parallelStream().flatMapToInt(group -> {
         MergeSequential.merge(group, 0, Math.max(0, group.length / 2 - 1), group.length - 1);
         return Arrays.stream(group);
-      }).toArray(), 0, arr.length);
+      }).toArray();
     }
     System.arraycopy(array, 0, arr, 0, arr.length);
   }
